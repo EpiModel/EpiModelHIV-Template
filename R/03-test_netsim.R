f56e99da29a771a3f8e3ada74dcfdc571020060f
Samuel Jenness <samuel.m.jenness@emory.edu>
2022-08-17 14:28:13 -0400

    Rename test-netsim

--- R/03-test_netsim.R
+++ R/03-test_netsim.R
@@ -2,18 +2,18 @@
 library(EpiModelHIV)
 
 # Necessary files
-epistats <- readRDS("data/netsim_inputs/epistats.rds")
-netstats <- readRDS("data/netsim_inputs/netstats.rds")
-est      <- readRDS("data/netsim_inputs/netest.rds")
+epistats <- readRDS("data/input/epistats.rds")
+netstats <- readRDS("data/input/netstats.rds")
+est      <- readRDS("data/input/netest.rds")
 
 # Parameters
-prep_start <- 65 * 52
+prep_start <- 2 * 52
 param <- param.net(
   data.frame.params = readr::read_csv("data/input/params.csv"),
   netstats          = netstats,
   epistats          = epistats,
   prep.start        = prep_start,
-  riskh.start       = prep_start - 53
+  riskh.start       = prep_start - 26
 )
 
 # Initial conditions
@@ -22,12 +22,12 @@ init <- init_msm()
 # Controls
 source("R/utils-targets.R")
 control <- control_msm(
-  nsteps              = 25,
+  nsteps              = 250,
   nsims               = 1,
   ncores              = 1,
-  cumulative.edgelist = TRUE,
+  cumulative.edgelist = FALSE,
   truncate.el.cuml    = 0,
-  .tracker.list       = calibration_trackers,
+  # .tracker.list       = calibration_trackers,
   verbose             = TRUE,
   raw.output          = FALSE
 )
