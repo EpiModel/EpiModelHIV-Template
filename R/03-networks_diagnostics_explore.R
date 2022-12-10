7804c1a6ef1c0a01786ab207e1ba9d26c696172e
Samuel Jenness <samuel.m.jenness@emory.edu>
2022-12-10 16:32:13 -0500

    Update diagnostics explore defaults

--- R/03-networks_diagnostics_explore.R
+++ R/03-networks_diagnostics_explore.R
@@ -11,19 +11,21 @@ library("EpiModelHIV")
 dx <- readRDS("data/intermediate/diagnostics/netdx-main.rds")
 print(dx$dx_main, digits = 2)
 plot(dx$dx_main)
+plot(dx$dx_main, type = "duration")
 
 print(dx$dx_main_static, digits = 2)
-plot(dx$dx_main_static)
+plot(dx$dx_main_static, sim.lines = TRUE, sim.lwd = 0.1)
 
 # Casual
 dx <- readRDS("data/intermediate/diagnostics/netdx-casl.rds")
 print(dx$dx_casl, digits = 2)
 plot(dx$dx_casl)
+plot(dx$dx_casl, type = "duration")
 
 print(dx$dx_casl_static, digits = 2)
-plot(dx$dx_casl_static)
+plot(dx$dx_casl_static, sim.lines = TRUE, sim.lwd = 0.1)
 
 # Inst
 dx <- readRDS("data/intermediate/diagnostics/netdx-inst.rds")
 print(dx$dx_inst, digits = 2)
-plot(dx$dx_inst)
+plot(dx$dx_inst, sim.lines = TRUE, sim.lwd = 0.1)
