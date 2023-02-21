055cdf03e8893271678ca3bf5d59fab12e38cdc5
Adrien Le Guillou <git@aleguillou.org>
2023-02-21 14:08:55 +0100

    ready for merging

--- R/utils-0_project_settings.R
+++ R/utils-0_project_settings.R
@@ -8,8 +8,8 @@ calib_dir <- "./data/intermediate/calibration/"
 scenarios_dir <- "./data/intermediate/scenarios/"
 
 # Information for the HPC workflows
-current_git_branch <- "calib_plots"
-mail_user <- "aleguil@emory.edu" # or any other mail provider
+current_git_branch <- "main"
+mail_user <- "USER@emory.edu" # or any other mail provider
 
 # Relevant time steps for the simulation
 calibration_end    <- 52 * 60
