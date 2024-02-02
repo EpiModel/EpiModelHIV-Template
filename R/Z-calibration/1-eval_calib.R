b33cf0f162079ff767e07ac636a32d77e9873f1a
Adrien Le Guillou <git@aleguillou.org>
2024-02-02 15:37:34 +0100

    add eval calib

--- R/Z-calibration/1-eval_calib.R
+++ R/Z-calibration/1-eval_calib.R
@@ -1,4 +1,9 @@
 # must be run after the workflow and downloading "calib_assess.csv"
+
+# Settings ---------------------------------------------------------------------
+source("R/shared_variables.R", local = TRUE)
+
+# Libraries --------------------------------------------------------------------
 library(dplyr)
 library(tidyr)
 library(ggplot2)
@@ -8,7 +13,8 @@ d_calib <- readr::read_csv(fs::path(calib_dir, "calib_assess.csv"))
 
 # look only at medians
 d_calib |>
-  select(scenario_name, ends_with("__q2"))
+  select(scenario_name, ends_with("__q2")) |>
+  as.list()
 
 d_calib |>
   select(scenario_name, starts_with("cc.dx"))
