f5b440db4bd8f7e35913b68292db7285adbc7146
Adrien Le Guillou <git@aleguillou.org>
2024-02-21 08:43:47 +0100

    update setup

--- R/00-setup.R
+++ R/00-setup.R
@@ -1,13 +1,27 @@
+## 0. Projet Initial Setup
+##
+## Activate `renv` and install the required packages
+
+# This script should be run in a fresh R session
+rs()
+
+# Setup ------------------------------------------------------------------------
+source("R/shared_variables.R", local = TRUE)
+
+# Process ----------------------------------------------------------------------
+#
 ## Setup `renv` for the project and install the necessary packages
+source("R/shared_variables.R", local = TRUE)
 
 # Initialize renv but do not install anything yet
 renv::init(bare = TRUE)
 
 # restart R
+rs()
 
 # This code installs the packages only available on GitHub (not CRAN ones)
 renv::install(c(
-  paste0("EpiModel/EpiModelHIV-p@", emhivp_branch),
+  paste0("EpiModel/EpiModelHIV-p@", EMHIVp_branch),
   "EpiModel/EpiModelHPC"
 ))
 
