e267ac37f73531955aee6ad7c0fddd47c28b7c72
Adrien Le Guillou <git@aleguillou.org>
2024-02-22 11:20:46 +0100

    test with post prep sti

--- R/hpc_configs.R
+++ R/hpc_configs.R
@@ -3,8 +3,8 @@
 ## This script should not be run directly. But `sourced` the from scripts that
 ## interact with the HPC (usually the `workflow-***.R` ones)
 
-current_git_branch <- "main"
-mail_user <- "user@emory.edu"
+current_git_branch <- "sti_cal"
+mail_user <- "aleguil@emory.edu"
 
 hpc_node_setup <- c(
   ". /projects/epimodel/spack/share/spack/setup-env.sh",
