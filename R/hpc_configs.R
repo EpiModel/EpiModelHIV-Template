d4777ea230b0ebaae43f134cbe0000c2292ccae5
Adrien Le Guillou <git@aleguillou.org>
2024-06-24 14:24:41 +0200

    up

--- R/hpc_configs.R
+++ R/hpc_configs.R
@@ -3,7 +3,7 @@
 ## This script should not be run directly. But `sourced` the from scripts that
 ## interact with the HPC (usually the `workflow-***.R` ones)
 
-current_git_branch <- "main"
+current_git_branch <- "tester"
 mail_user <- "<user>@emory.edu"
 
 hpc_node_setup <- c(
