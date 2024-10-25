69a2a27a050d21624a41fd9ec411e87fcad77c29
Adrien Le Guillou <git@aleguillou.org>
2024-10-25 11:03:43 +0200

    test all first try

---
+++ R/Z-calibration/workflow-test_all.R
@@ -0,0 +1,151 @@
+## HPC Workflow: test all
+# Setup ------------------------------------------------------------------------
+library(slurmworkflow)
+
+hpc_context <- TRUE
+source("R/shared_variables.R", local = TRUE)
+source("R/A-networks/z-context.R", local = TRUE)
+source("R/hpc_configs.R", local = TRUE)
+
+# Networks ---------------------------------------------------------------------
+wf <- make_em_workflow("test_all", override = TRUE)
+
+wf <- add_workflow_step(
+  wf_summary = wf,
+  step_tmpl = step_tmpl_do_call_script(
+    r_script = "R/A-networks/1-estimation.R",
+    args = list(hpc_context = TRUE),
+    setup_lines = hpc_node_setup
+  ),
+  sbatch_opts = list(
+    "cpus-per-task" = est_cores,
+    "time" = "24:00:00",
+    "mem" = "0"
+  )
+)
+
+wf <- add_workflow_step(
+  wf_summary = wf,
+  step_tmpl = step_tmpl_do_call_script(
+    r_script = "R/A-networks/2-diagnostics.R",
+    args = list(hpc_context = TRUE),
+    setup_lines = hpc_node_setup
+  ),
+  sbatch_opts = list(
+    "cpus-per-task" = est_cores,
+    "time" = "24:00:00",
+    "mem" = "0"
+  )
+)
+
+# Restart point single ---------------------------------------------------------
+hpc_context <- TRUE
+source("R/shared_variables.R", local = TRUE)
+source("R/C-netsim_scenarios/z-context.R", local = TRUE)
+source("R/hpc_configs.R", local = TRUE)
+
+wf <- add_workflow_step(
+  wf_summary = wf,
+  step_tmpl = step_tmpl_do_call_script(
+    r_script = "R/D-restart_point/1-restart_point_single.R",
+    args = list(hpc_context = TRUE),
+    setup_lines = hpc_node_setup
+  ),
+  sbatch_opts = list(
+    "cpus-per-task" = 1,
+    "time" = "04:00:00",
+    "mem-per-cpu" = "5G"
+  )
+)
+
+hpc_context <- TRUE
+source("R/shared_variables.R", local = TRUE)
+source("R/F-intervention_scenarios/z-context.R", local = TRUE)
+source("R/hpc_configs.R", local = TRUE)
+
+max_cores <- 8
+
+# Process ----------------------------------------------------------------------
+
+source("R/netsim_settings.R", local = TRUE)
+
+# Control settings
+control <- control_msm(
+  start               = restart_time,
+  nsteps              = intervention_end,
+  initialize.FUN      = reinit_msm,
+  verbose             = FALSE
+)
+
+# Define test scenarios
+scenarios_df <- readr::read_csv("data/input/scenarios.csv")
+scenarios_list <- EpiModel::create_scenario_list(scenarios_df)
+
+wf <- add_workflow_step(
+  wf_summary = wf,
+  step_tmpl = step_tmpl_netsim_scenarios(
+    path_to_restart, param, init, control,
+    scenarios_list = scenarios_list,
+    output_dir = scenarios_dir,
+    n_rep = 32,
+    n_cores = max_cores,
+    max_array_size = 500,
+    setup_lines = hpc_node_setup
+  ),
+  sbatch_opts = list(
+    "mail-type" = "FAIL,TIME_LIMIT",
+    "cpus-per-task" = max_cores,
+    "time" = "04:00:00",
+    "mem-per-cpu" = "5G"
+  )
+)
+
+# Process calibrations
+wf <- add_workflow_step(
+  wf_summary = wf,
+  step_tmpl = step_tmpl_merge_netsim_scenarios_tibble(
+      sim_dir = scenarios_dir,
+      output_dir = fs::path(scenarios_dir, "merged_tibbles"),
+      steps_to_keep = intervention_end - intervention_start,
+      cols = dplyr::everything(),
+      n_cores = max_cores,
+      setup_lines = hpc_node_setup
+    ),
+    sbatch_opts = list(
+      "mail-type" = "END",
+      "cpus-per-task" = max_cores,
+      "time" = "02:00:00",
+      "mem-per-cpu" = "5G"
+    )
+)
+
+# make tables step
+wf <- add_workflow_step(
+  wf_summary = wf,
+  step_tmpl = step_tmpl_do_call_script(
+    r_script = "R/F-intervention_scenarios/2-process_tables.R",
+    args = list(hpc_context = TRUE),
+    setup_lines = hpc_node_setup
+  ),
+  sbatch_opts = list(
+    "cpus-per-task" = max_cores,
+    "time" = "04:00:00",
+    "mem-per-cpu" = "5G"
+  )
+)
+
+# make plots step
+wf <- add_workflow_step(
+  wf_summary = wf,
+  step_tmpl = step_tmpl_do_call_script(
+    r_script = "R/F-intervention_scenarios/3-process_plots.R",
+    args = list(hpc_context = TRUE),
+    setup_lines = hpc_node_setup
+  ),
+  sbatch_opts = list(
+    "mail-type" = "END",
+    "cpus-per-task" = max_cores,
+    "time" = "04:00:00",
+    "mem-per-cpu" = "5G"
+  )
+)
