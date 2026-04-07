ff0d637b55850b13c87ba986fdc5d76e1895d2dd
Adrien Le Guillou <git@aleguillou.org>
2026-04-07 22:44:05 +0200

    working example

--- R/z-test.R
+++ R/z-test.R
@@ -29,8 +29,9 @@ add.edges.active(
   head = el$head,
   tail = el$tail,
   onset = el$start,
-  terminus = el$stop + 1L
-  # TODO: add network type on edges?
+  terminus = el$stop + 1L,
+  names.eval = rep(list("network"), nrow(el)),
+  vals.eval = lapply(el$network, \(x) list(network = x))
 )
 
 for (at in seq_along(sim$run$sim1$tracked_attributes)) {
@@ -45,8 +46,14 @@ for (at in seq_along(sim$run$sim1$tracked_attributes)) {
     }
     uids <- sim$run$sim1$tracked_attributes[[at]][[item]]$uid
     values <- sim$run$sim1$tracked_attributes[[at]][[item]]$value
-    activate.vertex.attribute(nw, item, v = uids, value = values,
-                              onset = at, terminus = Inf)
+    activate.vertex.attribute(
+      nw,
+      item,
+      v = uids,
+      value = values,
+      onset = at,
+      terminus = Inf
+    )
   }
 }
 
@@ -70,3 +77,114 @@ is.active(nw, at = 13, v = 10046)
 #     }
 #   }
 # }
+
+# With TergmLite ---------------------------------------------------------------
+pkgload::load_all("../../core_EM/EpiModel/")
+library(dplyr)
+library(ndtv)
+set.seed(1)
+
+# 1. Network setup + TERGM estimation
+n <- 1e3
+nw <- network_initialize(n = n)
+formation <- ~edges
+target.stats <- 0.4 * n
+coef.diss <- dissolution_coefs(dissolution = ~ offset(edges), duration = 10)
+est <- netest(nw, formation, target.stats, coef.diss)
+
+# 2. Epidemic simulation
+param <- param.net(inf.prob = 1)
+init <- init.net(i.num = 0.1 * n)
+control <- control.net(
+  type = "SI",
+  nsteps = 100,
+  nsims = 1,
+  tergmLite = TRUE,
+  tracked.attributes = c('status'),
+  cumulative.edgelist = TRUE,
+  truncate.el.cuml = Inf,
+  save.cumulative.edgelist = TRUE,
+  save.run = TRUE,
+  verbose = FALSE
+)
+system.time({
+  sim <- netsim(est, param, init, control)
+})
+
+# To network dynamic -----------------------------------------------------------
+n_nodes <- max(sim$run$sim1$attr$unique_id)
+n_steps <- control$nsteps
+
+nw <- network.initialize(n_nodes, directed = FALSE)
+el <- sim$cumulative.edgelist$sim1 |>
+  mutate(stop = ifelse(is.na(stop), sim$run$sim1$current_timestep, stop))
+
+add.edges.active(
+  nw,
+  head = el$head,
+  tail = el$tail,
+  onset = el$start,
+  terminus = el$stop + 1L,
+  names.eval = rep(list("network"), nrow(el)),
+  vals.eval = lapply(el$network, \(x) list(network = x))
+)
+
+for (at in seq_along(sim$run$sim1$tracked_attributes)) {
+  for (item in names(sim$run$sim1$tracked_attributes[[at]])) {
+    if (item == "active") {
+      on_pos <- which(sim$run$sim1$tracked_attributes[[at]][[item]]$value == 1)
+      off_pos <- which(sim$run$sim1$tracked_attributes[[at]][[item]]$value == 0)
+      uids <- sim$run$sim1$tracked_attributes[[at]][[item]]$uid[on_pos]
+      activate.vertices(nw, v = uids, onset = at, terminus = Inf)
+      uids <- sim$run$sim1$tracked_attributes[[at]][[item]]$uid[off_pos]
+      deactivate.vertices(nw, v = uids, onset = at, terminus = Inf)
+    } else {
+      uids <- sim$run$sim1$tracked_attributes[[at]][[item]]$uid
+      values <- sim$run$sim1$tracked_attributes[[at]][[item]]$value
+      activate.vertex.attribute(
+        nw,
+        item,
+        v = uids,
+        value = values,
+        onset = at,
+        terminus = Inf
+      )
+    }
+  }
+}
+
+orig_nw <- get_network(sim)
+
+# 3. Extract networkDynamic + color by infection status
+nw <- color_tea(nw, old.var = "status", verbose = FALSE)
+orig_nw <- color_tea(orig_nw, verbose = FALSE)
+
+# 4. Compute layout + render
+slice.par <- list(
+  start = 1,
+  end = 25,
+  interval = 1,
+  aggregate.dur = 1,
+  rule = "any"
+)
+compute.animation(nw, slice.par = slice.par)
+render.d3movie(
+  nw,
+  vertex.cex = 0.9,
+  vertex.col = "ndtvcol",
+  edge.col = "darkgrey",
+  vertex.border = "lightgrey",
+  displaylabels = FALSE,
+  filename = paste0(getwd(), "/movie.html")
+)
+
+compute.animation(orig_nw, slice.par = slice.par)
+render.d3movie(
+  orig_nw,
+  vertex.cex = 0.9,
+  vertex.col = "ndtvcol",
+  edge.col = "darkgrey",
+  vertex.border = "lightgrey",
+  displaylabels = FALSE,
+  filename = paste0(getwd(), "/movie.html")
+)
