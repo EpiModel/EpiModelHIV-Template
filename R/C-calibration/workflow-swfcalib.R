3d7898b40a77f26d049416b5662d2d76d829cd4d
Adrien Le Guillou <git@aleguillou.org>
2026-07-28 12:06:33 +0200

    update swfcalib tests

--- R/C-calibration/workflow-swfcalib.R
+++ R/C-calibration/workflow-swfcalib.R
@@ -54,7 +54,8 @@ wf <- add_workflow_step(
     MoreArgs = list(
       n_cores = batch_size,
       n_batches = max(batch_numbers),
-      calib_object = calib_object
+      calib_object = calib_object,
+      future_use_plan = future::tweak("multicore", workers = batch_size)
     )
   ),
   sbatch_opts = list(
