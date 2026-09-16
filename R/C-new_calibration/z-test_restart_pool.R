050ac58ba63677fa42c0bb7a58244f6fc94ea7e5
Adrien Le Guillou <git@aleguillou.org>
2026-09-16 14:36:28 +0200

    updates to EM

--- R/C-new_calibration/z-test_restart_pool.R
+++ R/C-new_calibration/z-test_restart_pool.R
@@ -8,35 +8,14 @@ library(dplyr)
 source("R/shared_variables.R", local = TRUE)
 source("R/C-calibration/z-context.R", local = TRUE)
 
-make_restart_point_hiv <- function(sim, sim_num, sim_cost = Inf) {
-  attrs_names <- names(EpiModelHIV::get_default_attrs())
-  time_prefixes <- c(".last$", ".time$")
-
-  time_attrs <- Reduce(
-    function(a, prefix) c(a, grepv(prefix, attrs_names)),
-    time_prefixes,
-    init = character(0)
-  )
-
-  restart_point <- EpiModel::make_restart_point(
-    sim,
-    time_attrs,
-    sim_num,
-    keep_steps = 1
-  )
-
-  restart_point[["_calibration_cost"]] <- sim_cost
-  restart_point
-}
-
 # Load files -------------------------------------------------------------------
 sim_files <- fs::dir_ls("./data/run/calibration/", regexp = "sim__.*")
 sim_res <- lapply(sim_files, readRDS)
 # Ensure they all have the same control
-sim_res[[1]]$control$save.other <- c(
-  "run",
-  "coef.form"
-)
+# sim_res[[1]]$control$save.other <- c(
+#   "run",
+#   "coef.form"
+# )
 for (i in 2:length(sim_res)) {
   sim_res[[i]]$control <- sim_res[[1]]$control
 }
@@ -61,4 +40,4 @@ restart_pool <- EpiModel::make_restart_point(
 )
 saveRDS(restart_pool, "restart_pool.rds")
 
-str(restart_pool$run, max.level = 1)
+str(sims$raw.records, max.level = 1)
